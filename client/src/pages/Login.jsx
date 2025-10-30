25f962ed7103d78e80367055e750d123118a4aae
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-10-30 09:20:49 -0400

    update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,9 @@ import { User2Icon, MailIcon, LockIcon } from "lucide-react";
 import React from "react";
 
 const Login = () => {
-  const [state, setState] = React.useState("login");
+  const query = new URLSearchParams(window.location.search);
+  const urlState = query.get("state");
+  const [state, setState] = React.useState(urlState || "login");
 
   const [formData, setFormData] = React.useState({
     name: "",
