31d990ff04dd1b82091d664edbf133f8ad2f076d
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-11-04 02:29:21 -0500

    Update Footer.jsx

--- client/src/components/Home/Footer.jsx
+++ client/src/components/Home/Footer.jsx
@@ -1,179 +1,69 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <>
-      <footer className="flex flex-wrap justify-center lg:justify-between overflow-hidden gap-10 md:gap-20 py-16 px-6 md:px-16 lg:px-24 xl:px-32 text-[13px] text-gray-500 bg-gradient-to-r via-indigo-200/60 to-white mt-40">
-        <div className="flex flex-wrap items-start gap-10 md:gap-[60px] xl:gap-[140px]">
-          <a href="https://prebuiltui.com">
-            <img src="/logo.svg" alt="logo" className="h-11 w-auto" />
-          </a>
-          <div>
-            <p className="text-slate-100 font-semibold">Product</p>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Support
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Affiliate
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="text-slate-100 font-semibold">Resources</p>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Company
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Blogs
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Community
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Careers
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="text-slate-100 font-semibold">Legal</p>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Privacy
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-indigo-600 transition">
-                  Terms
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="flex flex-col max-md:items-center max-md:text-center gap-2 items-end">
-          <p className="max-w-60">
-            Making professional resume building accessible to everyone.
-          </p>
-          <div className="flex items-center gap-4 mt-3">
-            <a href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-dribbble size-5 hover:text-indigo-500"
-                aria-hidden="true"
-              >
-                <circle cx="12" cy="12" r="10"></circle>
-                <path d="M19.13 5.09C15.22 9.14 10 10.44 2.25 10.94"></path>
-                <path d="M21.75 12.84c-6.62-1.41-12.14 1-16.38 6.32"></path>
-                <path d="M8.56 2.75c4.37 6 6 9.42 8 17.72"></path>
-              </svg>
-            </a>
-            <a href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-linkedin size-5 hover:text-indigo-500"
-                aria-hidden="true"
-              >
-                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                <rect width="4" height="12" x="2" y="9"></rect>
-                <circle cx="4" cy="4" r="2"></circle>
-              </svg>
-            </a>
-            <a href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-twitter size-5 hover:text-indigo-500"
-                aria-hidden="true"
-              >
-                <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-              </svg>
-            </a>
-            <a href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-youtube size-6 hover:text-indigo-500"
-                aria-hidden="true"
-              >
-                <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"></path>
-                <path d="m10 15 5-3-5-3z"></path>
-              </svg>
-            </a>
-          </div>
-          <p className="mt-3 text-center">
-            © 2025 <a href="https://prebuiltui.com">AI-resume Builder</a>
-          </p>
-        </div>
-      </footer>
-
-      <style>{`
-                @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-            
-                * {
-                    font-family: 'Poppins', sans-serif;
-                }
-            `}</style>
-    </>
-  );
+   return (
+     <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500 mt-10 mb-5">
+       <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
+         <div className="md:max-w-96">
+           <svg
+             width="157"
+             height="40"
+             viewBox="0 0 157 40"
+             fill="none"
+             xmlns="http://www.w3.org/2000/svg"
+           >
+             <path
+               d="M47.904 28.28q-1.54 0-2.744-.644a5.1 5.1 0 0 1-1.904-1.82q-.672-1.148-.672-2.604v-3.864q0-1.456.7-2.604a4.9 4.9 0 0 1 1.904-1.792q1.204-.672 2.716-.672 1.82 0 3.276.952a6.44 6.44 0 0 1 2.324 2.52q.868 1.567.868 3.556 0 1.96-.868 3.556a6.5 6.5 0 0 1-2.324 2.492q-1.456.924-3.276.924m-7.196 5.32V14.56h3.08v3.612l-.532 3.276.532 3.248V33.6zm6.692-8.232q1.12 0 1.96-.504a3.6 3.6 0 0 0 1.344-1.456q.504-.924.504-2.128t-.504-2.128a3.43 3.43 0 0 0-1.344-1.428q-.84-.532-1.96-.532t-1.988.532a3.43 3.43 0 0 0-1.344 1.428q-.476.924-.476 2.128t.476 2.128a3.6 3.6 0 0 0 1.344 1.456q.868.504 1.988.504M56.95 28V14.56h3.08V28zm3.08-7.476-1.064-.532q0-2.548 1.12-4.116 1.148-1.596 3.444-1.596 1.008 0 1.82.364.812.365 1.512 1.176l-2.016 2.072a2.1 2.1 0 0 0-.812-.56 3 3 0 0 0-1.036-.168q-1.287 0-2.128.812-.84.811-.84 2.548m14.156 7.756q-2.016 0-3.64-.896a7 7 0 0 1-2.548-2.52q-.924-1.596-.924-3.584t.924-3.556a6.87 6.87 0 0 1 2.492-2.52q1.596-.924 3.528-.924 1.876 0 3.304.868a6.05 6.05 0 0 1 2.268 2.38q.84 1.512.84 3.444 0 .336-.056.7a7 7 0 0 1-.112.756H69.23v-2.52h9.436l-1.148 1.008q-.056-1.232-.476-2.072a3 3 0 0 0-1.204-1.288q-.756-.448-1.876-.448-1.176 0-2.044.504a3.43 3.43 0 0 0-1.344 1.428q-.476.896-.476 2.156t.504 2.212 1.428 1.484q.924.504 2.128.504 1.037 0 1.904-.364a4 4 0 0 0 1.512-1.064l1.96 1.988a6.3 6.3 0 0 1-2.38 1.736 7.6 7.6 0 0 1-2.968.588m15.91 0q-1.54 0-2.745-.644a5.1 5.1 0 0 1-1.904-1.82q-.672-1.148-.672-2.604v-3.864q0-1.456.7-2.604a4.9 4.9 0 0 1 1.904-1.792q1.204-.672 2.716-.672 1.821 0 3.276.952a6.44 6.44 0 0 1 2.324 2.52q.869 1.567.868 3.556 0 1.96-.868 3.556a6.5 6.5 0 0 1-2.324 2.492q-1.455.924-3.276.924M82.898 28V7.84h3.08v10.024l-.532 3.248.532 3.276V28zm6.692-2.632q1.12 0 1.96-.504a3.6 3.6 0 0 0 1.344-1.456q.504-.924.504-2.128t-.504-2.128a3.43 3.43 0 0 0-1.344-1.428q-.84-.532-1.96-.532t-1.988.532a3.43 3.43 0 0 0-1.344 1.428q-.476.924-.476 2.128.001 1.204.476 2.128a3.6 3.6 0 0 0 1.344 1.456q.87.504 1.988.504m15.067 2.912q-1.708 0-3.052-.756a5.5 5.5 0 0 1-2.072-2.072q-.728-1.344-.728-3.08V14.56h3.08v7.672q0 .98.308 1.68.336.672.952 1.036.644.364 1.512.364 1.344 0 2.044-.784.728-.812.728-2.296V14.56h3.08v7.812q0 1.764-.756 3.108a5.3 5.3 0 0 1-2.044 2.072q-1.317.728-3.052.728m8.976-.28V14.56h3.08V28zm1.54-15.904q-.783 0-1.316-.532-.504-.532-.504-1.316t.504-1.316a1.8 1.8 0 0 1 1.316-.532q.813 0 1.316.532t.504 1.316q0 .784-.504 1.316t-1.316.532M120.169 28V7.84h3.08V28zm8.552 0V8.96h3.08V28zm-3.22-10.64v-2.8h9.52v2.8zm17.274 10.92q-1.708 0-3.052-.756a5.5 5.5 0 0 1-2.072-2.072q-.728-1.344-.728-3.08V14.56h3.08v7.672q0 .98.308 1.68.336.672.952 1.036.643.364 1.512.364 1.344 0 2.044-.784.728-.812.728-2.296V14.56h3.08v7.812q0 1.764-.756 3.108a5.3 5.3 0 0 1-2.044 2.072q-1.317.728-3.052.728m8.977-.28V14.56h3.08V28zm1.54-15.904q-.785 0-1.316-.532-.504-.532-.504-1.316t.504-1.316a1.8 1.8 0 0 1 1.316-.532q.812 0 1.316.532t.504 1.316-.504 1.316-1.316.532"
+               fill="#000"
+             />
+             <path
+               d="m8.75 11.3 6.75 3.884 6.75-3.885M8.75 34.58v-7.755L2 22.939m27 0-6.75 3.885v7.754M2.405 15.408 15.5 22.954l13.095-7.546M15.5 38V22.939M29 28.915V16.962a2.98 2.98 0 0 0-1.5-2.585L17 8.4a3.01 3.01 0 0 0-3 0L3.5 14.377A3 3 0 0 0 2 16.962v11.953A2.98 2.98 0 0 0 3.5 31.5L14 37.477a3.01 3.01 0 0 0 3 0L27.5 31.5a3 3 0 0 0 1.5-2.585"
+               stroke="#4F39F6"
+               strokeWidth="2.5"
+               strokeLinecap="round"
+               strokeLinejoin="round"
+             />
+           </svg>
+           <p className="mt-6 text-sm">
+             Lorem Ipsum is simply dummy text of the printing and typesetting
+             industry. Lorem Ipsum has been the industry's standard dummy text
+             ever since the 1500s, when an unknown printer took a galley of type
+             and scrambled it to make a type specimen book.
+           </p>
+         </div>
+         <div className="flex-1 flex items-start md:justify-end gap-20">
+           <div>
+             <h2 className="font-semibold mb-5 text-gray-800">Company</h2>
+             <ul className="text-sm space-y-2">
+               <li>
+                 <a href="#">Home</a>
+               </li>
+               <li>
+                 <a href="#">About us</a>
+               </li>
+               <li>
+                 <a href="#">Contact us</a>
+               </li>
+               <li>
+                 <a href="#">Privacy policy</a>
+               </li>
+             </ul>
+           </div>
+           <div>
+             <h2 className="font-semibold mb-5 text-gray-800">Get in touch</h2>
+             <div className="text-sm space-y-2">
+               <p>+1-212-456-7890</p>
+               <p>contact@example.com</p>
+             </div>
+           </div>
+         </div>
+       </div>
+       <p className="pt-4 text-center text-xs md:text-sm pb-5">
+         Copyright 2024 © <a href="https://prebuiltui.com">PrebuiltUI</a>. All
+         Right Reserved.
+       </p>
+     </footer>
+   );
 };
 
 export default Footer;
